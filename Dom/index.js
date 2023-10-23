dc06771fe363b75c6d557fb909c5f27425d80906
KrushnaKale07 <krushnakale0007@gmail.com>
2023-10-23 18:20:52 +0530

    resume added

---
+++ Dom/index.js
@@ -0,0 +1,26 @@
+console.log('Script Loaded'); 
+
+// function fun1() {
+//     var demo = [1,2,3,4]
+//     const sample =[4,3,2,1]
+//     sample.push(5)
+//     console.log(sample);
+// }
+// fun1()
+
+// demo.push(5)
+// const brr = [11,12,13]
+// var arr = [1,2,3,4,5]
+// console.log(arr);
+
+// console.log(document.getElementById('Logo'));
+
+// console.log(document.getElementsByClassName('blog-card'));
+
+// console.log(document.getElementsByTagName('img'));
+
+console.log(document.querySelectorAll('.logo img'));
+console.log(document.querySelector('.logo img'));
+
+// brr.push(1,13);
+// console.log(brr);
