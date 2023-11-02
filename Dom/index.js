c2043977c110abf822d97b1a16f539f273a2d57e
KrushnaKale07 <krushnakale0007@gmail.com>
2023-11-02 19:50:57 +0530

    back image update

--- Dom/index.js
+++ Dom/index.js
@@ -1,5 +1,14 @@
 console.log('Script Loaded'); 
 
+// console.log(document.getElementById("topbar"));
+
+// console.log(document.getElementsByClassName("menu-item"));  
+
+console.log(document.querySelectorAll('#topbar'));
+console.log(document.querySelector('#topbar'));
+
+
+//  
 // function fun1() {
 //     var demo = [1,2,3,4]
 //     const sample =[4,3,2,1]
@@ -10,7 +19,7 @@ console.log('Script Loaded');
 
 // demo.push(5)
 // const brr = [11,12,13]
-// var arr = [1,2,3,4,5]
+// var arr = [1,2,3,4,5] 
 // console.log(arr);
 
 // console.log(document.getElementById('Logo'));
@@ -29,4 +38,4 @@ console.log('Script Loaded');
 
 // var CountdownElement = document.getElementById('countdown');
 
-// console.log(CountdownElement.innerHTML);
+// console.log(CountdownElement.innerHTML); 
---
+++ Dom/update.js
@@ -0,0 +1,25 @@
+console.log('js Loaded');
+
+var CountdownElement = document.getElementById('countdown');
+var bgImageElement = document.getElementById('bg-image')
+
+// console.log(bgImageElement.src);
+// console.log(CountdownElement.innerHTML);
+
+var initialCountdownVal = CountdownElement.innerHTML;
+
+setInterval(function () {
+
+    initialCountdownVal = initialCountdownVal > 0 ? initialCountdownVal - 1 : 0;
+ 
+    CountdownElement.innerHTML=initialCountdownVal ;
+
+    var backImgpath =initialCountdownVal %2 === 0 ? '/Dom/image/Krushna Kale Selfie before test.jpeg' : '/Dom/image/img1.jpg'
+
+    bgImageElement.src = backImgpath;
+
+    bgImageElement.className = 'BackImage';
+
+    console.log('bgImageElement');
+},1000);
+ 
